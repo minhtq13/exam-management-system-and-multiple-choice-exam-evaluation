fd39e8ae26a34eb4f3a54428fafc31c18a23b4e3
chiendao1808 <chiendao1808@gmail.com>
2024-01-26 22:44:47 +0700

    ELEARNING: add func init application components

--- be_java_elearning/elearning_support/src/main/java/com/elearning/elearning_support/ElearningSupportApplication.java
+++ be_java_elearning/elearning_support/src/main/java/com/elearning/elearning_support/ElearningSupportApplication.java
@@ -1,13 +1,34 @@
 package com.elearning.elearning_support;
 
+import java.io.File;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import com.elearning.elearning_support.utils.file.FileUtils;
+import lombok.extern.slf4j.Slf4j;
 
+@EnableAsync
 @SpringBootApplication
+@Slf4j
 public class ElearningSupportApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElearningSupportApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // set time zone default
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        // init shared app folder
+        File sharedFolder = new File(FileUtils.getSharedAppDirectoryPath());
+        if (!sharedFolder.exists()) {
+            sharedFolder.mkdirs();
+        }
+        log.info("========= INITIALIZED SHARED FOLDER =========");
     }
 
 }
