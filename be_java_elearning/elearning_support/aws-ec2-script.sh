c20ef8f2504ad85b4068a913d700d20601dd0077
chiendao1808 <chiendao1808@gmail.com>
2024-02-14 22:16:02 +0700

    ELEARNING: add run bash script to build and run container

---
+++ be_java_elearning/elearning_support/aws-ec2-script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# remove current container and image
+docker rm -f elearning_support_backend
+docker rmi elearning_support_backend:1.0.0
+# build image
+docker build --tag=elearning_support_backend:1.0.0 .
+# run container
+docker compose -f docker-compose-aws.yml up -d
+# logs
+docker logs elearning_support_backend --tail 500 -f
