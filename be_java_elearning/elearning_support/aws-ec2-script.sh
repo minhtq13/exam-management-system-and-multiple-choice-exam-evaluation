d8735a9f1d9549892efd7151b59a91812137e7d9
chiendao1808 <chiendao1808@gmail.com>
2024-02-14 22:41:11 +0700

    ELEARNING: edit run script

--- be_java_elearning/elearning_support/aws-ec2-script.sh
+++ be_java_elearning/elearning_support/aws-ec2-script.sh
@@ -3,7 +3,7 @@
 docker rm -f elearning_support_backend
 docker rmi elearning_support_backend:1.0.0
 # build image
-docker build --tag=elearning_support_backend:1.0.0 .
+docker build -f ./Dockerfile --tag=elearning_support_backend:1.0.0 .
 # run container
 docker compose -f docker-compose-aws.yml up -d
 # logs
