e7d25f66d207f1a90d3b94dfc3bd1c9f0923cfb2
chiendao1808 <chiendao1808@gmail.com>
2024-02-19 01:01:07 +0700

    ELEARNING: config Docker FE build script

--- fe_react/local-build-script.sh
+++ fe_react/local-build-script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # remove current container and image
 docker rm -f elearning_support_fe
-docker rmi elearning_support_fe:1.0.0
+docker rmi leopepe2012/elearning_support_fe:latest
 # rebuild image
-docker build -f ./Dockerfile --tag=elearning_support_fe:1.0.0 .
+docker build -f ./Dockerfile --tag=leopepe2012/elearning_support_fe:latest .
 # run the container using docker-compose
 docker compose -f docker-compose-local.yml up -d
 # view logs after running the container
